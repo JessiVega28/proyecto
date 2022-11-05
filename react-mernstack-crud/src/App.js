8a7411ac7251e7916ed6e5ff0f7ff06ba39849fe
jessicavega <jessica.vega.iri@gmail.com>
2022-11-04 20:31:51 -0500

    Clase 04 Noviembre

---
+++ react-mernstack-crud/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+
+function App() {
+  return (
+    <div>
+    <h1>Hola</h1>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ react-mernstack-crud/src/components/create-student.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CreateStudent extends Component {
+  render() {
+    return (
+      <div>CreateStudent</div>
+    )
+  }
+}
